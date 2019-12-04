60be6f2441cf4adb893e51fcd7fabf1c4bb13dc1
StafaH <mhaiderbhai@hotmail.com>
2019-12-04 01:04:30 -0500

    Fix build.bat file and clean up initial empty project

--- misc/build.bat
+++ misc/build.bat
@@ -1,10 +1,9 @@
-::@echo off
+@echo off
 
-:: Uncomment to debug the vsvarsall call
-set VSCMD_DEBUG=1
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+:: This batch file requires a developer command prompt as shown shell.bat 
+:: Compiling is substantially faster if you open your editor using the developer cmd instead of calling vcvarsall.bat
 
- :: %~dp0 Cool way of getting the d = drive and p = path name of the current batch file.
+:: %~dp0 Cool way of getting the d = drive and p = path name of the current batch file.
 set BuildDir=%~dp0..\build
  
 if not exist %BuildDir% mkdir %BuildDir%
@@ -14,22 +13,37 @@ pushd %BuildDir%
 :: delete pdb file and get rid of output errors
 :: del *.pdb > NUL 2> NUL
  
-:: compiler flags:
-:: /Zi enable debugging information
-:: /FC use full path in diagnostics
-:: /I look for other include directories
-:: /W# Warnings
-set CommonCompilerFlags=/O2 /nologo /Zi /FC /I"..\deps\include" /W4 /MD -DIMGUI_IMPL_OPENGL_LOADER_GLEW
+set CommonCompilerFlags=/O2 /nologo -fp:fast -GR- /MP /Zi /FC /EHsc /I"..\deps\include" /W1 /MD
+set CommonCompilerFlags= /std:c++latest -DIMGUI_IMPL_OPENGL_LOADER_GLEW %CommonCompilerFlags% 
+
+
 set CommonLinkerFlags=user32.lib gdi32.lib winmm.lib opengl32.lib kernel32.lib shell32.lib glu32.lib
 set GLFWLibPath=/LIBPATH:..\deps\lib\GLFW\lib-vc2017 glfw3.lib
-set GLEWLibPath=..\deps\lib\GLEW\glew32s.lib ..\deps\lib\GLEW\glew32.lib
+set GLEWLibPath=/LIBPATH:..\deps\lib\GLEW glew32s.lib glew32.lib
 
 :: Code and libraries directory path
 set CodePath=..\code\*.cpp
-set ImGuiPath=..\code\imgui\*.cpp
 
-cl %CommonCompilerFlags% %CodePath% %ImGuiPath% /link %CommonLinkerFlags% %GLEWLibPath% %GLFWLibpath% /Fe:"dcmbrowser"
+:: Compile and Link
+cl %CommonCompilerFlags% %CodePath% %ImGuiPath% %tfdPath% /link %CommonLinkerFlags% %GLEWLibPath% %GLFWLibpath% /out:"main.exe"
  
-:: Copy dependencies if needed
- 
+:: Copy dependencies if needed (dll, config)
+
+
+
+echo .........
+echo Build Finished!
 popd
+
+:: Compiler Options Explained
+:: /O2 = Creates fast code.
+:: /nologo = does not display copyright banner during compilation
+:: /fp = floating point calculation options
+:: /Zi = enable debugging information
+:: /FC = use full path in diagnostics
+:: /I = look for other include directories
+:: /W# = Warnings
+:: /MD = Which runtime library to use (Static/Dynamic)
+:: /GR = enable run-time type information
+::
+::
--- misc/shell.bat
+++ misc/shell.bat
@@ -1,5 +1,7 @@
 @echo off
 
 :: Uncomment to debug the vsvarsall call
-::set VSCMD_DEBUG=1
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+:: set VSCMD_DEBUG=1
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+
+call Code
